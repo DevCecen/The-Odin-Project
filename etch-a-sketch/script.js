23ac37e0b6e5d7269f40c384305a8267554a86de
DevCecen <191208060+DevCecen@users.noreply.github.com>
2025-06-15 12:40:52 +0300

    Add mouseClik and paint functions

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 let squareValue = 16;
 let squareSize = 1920 / squareValue;
+let mouseIsDown = false;
 
 function generateGrid(){
     for(let i=0; i<(squareValue**2); i++)
@@ -13,3 +14,24 @@ function generateGrid(){
     }
 }
 
+function paint()
+{
+    const squares = document.querySelectorAll(".squares");
+    squares.forEach(square =>{
+        square.addEventListener("mouseover",()=>{
+            if(mouseIsDown == true)
+            {
+                square.style.backgroundColor = "black";
+            }
+        })
+    })
+}
+
+function mouseClick(){
+    document.body.addEventListener("mousedown",()=>{
+        mouseIsDown = true;
+    })
+    document.body.addEventListener("mouseup",()=>{
+        mouseIsDown = false;
+    })
+}
